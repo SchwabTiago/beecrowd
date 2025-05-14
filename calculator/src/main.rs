abfe3b373008e046c0b66ccfd16ee5517d78101d
SchwabTiago <tiagoschwabbs@gmail.com>
2025-05-13 22:37:42 -0300

    Calculadora finalizada

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,19 +1,50 @@
-use colour::green_ln;
+use colour::{green_ln, yellow_ln};
 use std::io;
 
 fn main() {
     loop {
-        green_ln!("---- Calculator ----");
-        println!("1- Sum");
-        println!("2- Sub");
-        println!("3- Mult");
-        println!("4- Div");
-        let mut menu_selected = String::new();
+        yellow_ln!("--- Calculadora ---");
+        print!("\n");
+        print!("\n");
+        green_ln!("---- Escolha uma opção ----");
+        println!("1 - Soma");
+        println!("2 - Subtração");
+        println!("3 - Multiplicação");
+        println!("4 - Divisão");
 
+        let mut option_input = String::new();
         io::stdin()
-            .read_line(&mut menu_selected)
-            .expect("Failed to read the line!!");
+            .read_line(&mut option_input)
+            .expect("Erro ao ler a opção");
 
-        println!("{}", menu_selected);
+        let option: i32 = option_input.trim().parse().expect("Opção inválida");
+
+        println!("Digite o primeiro número:");
+        let mut num1_input = String::new();
+        io::stdin()
+            .read_line(&mut num1_input)
+            .expect("Erro ao ler o primeiro número");
+        let num1: f64 = num1_input.trim().parse().expect("Número inválido");
+
+        println!("Digite o segundo número:");
+        let mut num2_input = String::new();
+        io::stdin()
+            .read_line(&mut num2_input)
+            .expect("Erro ao ler o segundo número");
+        let num2: f64 = num2_input.trim().parse().expect("Número inválido");
+
+        match option {
+            1 => println!("Resultado: {}", num1 + num2),
+            2 => println!("Resultado: {}", num1 - num2),
+            3 => println!("Resultado: {}", num1 * num2),
+            4 => {
+                if num2 == 0.0 {
+                    println!("Erro: divisão por zero!");
+                } else {
+                    println!("Resultado: {}", num1 / num2);
+                }
+            }
+            _ => println!("Opção inválida."),
+        }
     }
 }
